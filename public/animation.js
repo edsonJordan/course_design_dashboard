7baf632558e8bcf06c3434225b083b37d177bf77
Edson Jordan <edson_4555@hotmail.com>
2021-07-16 21:54:29 -0500

    adding hover from links navs

--- public/animation.js
+++ public/animation.js
@@ -9,16 +9,31 @@ document.querySelector('.selector-active').style.top = picks[0].offsetTop + "px"
 document.querySelector('.selector-active').style.left = picks[0].offsetLeft + "px";
 document.querySelector('.selector-active').style.height = activeWidthVerticalHeight + "px";
 document.querySelector('.selector-active').style.width = activeWidthVerticalWidth + "px";
+
 document.getElementById('accordian')?.addEventListener('click', (e)=>{
-    console.log(e.target.nodeName);
-    if(e.target.nodeName == 'A'){
-        let data = document.getElementById('accordian');
+    let data = document.getElementById('accordian');
+    if(e.target.nodeName == 'A'){                      
         data.getElementsByClassName('active')[0].classList.remove('active');
         const list = e.target.parentNode;
+        console.log("A",list);
         list.classList.add('active');
         document.querySelector('.selector-active').style.top = picks[0].offsetTop + "px";
         document.querySelector('.selector-active').style.left = picks[0].offsetLeft + "px";
         document.querySelector('.selector-active').style.height = activeWidthVerticalHeight + "px";
-        document.querySelector('.selector-active').style.width = activeWidthVerticalWidth + "px";
-    }    
+        document.querySelector('.selector-active').style.width = activeWidthVerticalWidth+ "px";     
+    }
+    if (e.target.nodeName == 'svg') {
+        data.getElementsByClassName('active')[0].classList.remove('active');
+        const list = e.target.parentNode.parentNode;
+        console.log('svg', list);
+        list.classList.add('active');
+        document.querySelector('.selector-active').style.top = picks[0].offsetTop + "px";
+        document.querySelector('.selector-active').style.left = picks[0].offsetLeft + "px";
+        document.querySelector('.selector-active').style.height = activeWidthVerticalHeight + "px"; 
+        document.querySelector('.selector-active').style.width = activeWidthVerticalWidth+ "px";  
+    }
+    /* if(e.target.nodeName == 'svg'){
+        console.log(svg);
+    } */
+      
 });
